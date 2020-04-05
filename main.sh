0e831ddb3f487e5a01484413dddb6e9c8012d006
pgd988 <pgd988@gmail.com>
2020-04-05 06:56:57 +0300

    v0.1

--- main.sh
+++ main.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 C=0
-REGISTRY="gcr.io/deep-wares-144610"
 IFS=$'\n\t'
-set -eou pipefail
+#set -eou pipefail
+set -eo pipefail
 IMAGE=`cat ./images.lst`
+print_help () {
+    echo -e "\nPlease call '$0 -p=\"Yor GCP Project ID\" -t=\"Threshold for Image deletion\" -k=\"Path to ServiceAccount json Key File\""
+}
 
-gcloud auth activate-service-account --key-file=./gcr.key --project deep-wares-144610
+if [ -z "$1" ]; then
+    print_help
+    exit 1
+fi
 
+### Args parcing
+for ARGUMENT in "$@"
+do
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    VALUE=$(echo $ARGUMENT | cut -f2 -d=)
+    case "$KEY" in
+            -p)           REGISTRY="gcr.io/${VALUE}" PROJECT=${VALUE};;
+            -t)           THRESHOLD=${VALUE} ;;
+            -k)           KEYFILE="${VALUE}" ;;
+            *)
+    esac
+done
+
+gcloud auth activate-service-account --key-file=$KEYFILE --project $PROJECT
 for img in $IMAGE; do
-COUNTALL="$(gcloud container images list-tags ${img} --limit=999999 --sort-by=TIMESTAMP  | grep -v DIGEST | wc -l)"
-
+CONTCOUNT="$(gcloud container images list-tags ${img} --limit=999999 --sort-by=TIMESTAMP  | grep -v DIGEST | wc -l)"
   for digest in $(gcloud container images list-tags ${img} --limit=999999 --sort-by=TIMESTAMP --format='get(digest)'); do
-
-    if [ $(( $COUNTALL - $C )) -ge 3 ]
+    if [ $(( $CONTCOUNT - $C )) -ge $THRESHOLD ]
     then
       (
         set -x
@@ -20,7 +38,7 @@ COUNTALL="$(gcloud container images list-tags ${img} --limit=999999 --sort-by=TI
       )
       let C=C+1
     else
-      echo "Deleted $(( $COUNTALL - 2 )) images in ${img}." >&2
+      echo "Deleted $(( $CONTCOUNT - $(($THRESHOLD - 1)) )) images in ${img}." >&2
       break
     fi
   done
